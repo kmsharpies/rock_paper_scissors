8a19e1d30c14d4790df3d7bfb5b3d9ff375d5c2d
kmsharpies <kiera.murraysharpe@gmail.com>
2024-05-21 16:41:42 +0100

    written code!

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class App {
 
     public static void main(String[] args) {
-        
+
+        Random random = new Random();
+        Scanner scanner =new Scanner(System.in);
+        Game game = new Game();
+        User player = new User();
+        Computer computer = new Computer();
+
+        player.makeMove();
+        computer.makeMove();
+
+        game.whoWins(player);
+
     }
+
+
 }
+
